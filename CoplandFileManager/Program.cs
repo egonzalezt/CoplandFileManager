c8d4e7788bd375e589da8104906f46efb3cd95ba
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-12 18:20:17 -0500

    fix(dotnet): add env support

--- CoplandFileManager/Program.cs
+++ CoplandFileManager/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddServices(builder.Configuration);
+builder.Configuration.AddEnvironmentVariables();
 
 var app = builder.Build();
 
